a1576ec1a3c42f4a1562d9b9e0e645e55000286f
asilaahr <a710230069@student.ums.ac.id>
2024-07-05 05:53:54 +0700

    Create bebekdanayam.py

---
+++ day26/bebekdanayam.py
@@ -0,0 +1,20 @@
+class Hewan:
+  def __init__(self, nama):
+    self.nama = nama
+
+  def bersuara(self):
+    print(f"{self.nama} bersuara!")
+
+class Bebek(Hewan):
+  def bersuara(self):
+    print(f"{self.nama} kwek-kwek!")
+
+class Ayam(Hewan):
+  def bersuara(self):
+    print(f"{self.nama} kukuruyuk!")
+
+bebek = Bebek("bebek")
+ayam = Ayam("ayam")
+
+bebek.bersuara()
+ayam.bersuara()
